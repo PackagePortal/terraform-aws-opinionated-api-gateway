11dab127fa24a2db4023a78b711dc77b0e45eae9
Benjamin Jewkes <bcj1111@gmail.com>
2022-06-07 16:49:48 -0400

    Add lambda module, fix IAM issue when > 10 things

--- main.tf
+++ main.tf
@@ -9,20 +9,22 @@ locals {
     endpoint : ""
     arn : ""
     method : "POST"
-    load_balancer_link_arn : "",
     cache : false
     image_hosting : false
-    key: ""
+    key: "",
+    name: ""
   }
   resource_count    = length(local.mappings)
   mappings          = [for mapping in var.mappings : merge(local.default_mapping, mapping)] # Sets defaults if not present
   needs_lambda      = contains(local.mappings.*.use_custom_auth, true)
   has_custom_domain = var.domain != ""
+  distinct_lambdas  = distinct(local.lambda_mappings.*.name)
 
   # Filtered lists of mappings by submodule they create
   s3_mappings     = [for item in local.mappings : item if lower(item.type) == "s3"]
   proxy_mappings  = [for item in local.mappings : item if lower(item.type) == "proxy"]
   sns_mappings    = [for item in local.mappings : item if lower(item.type) == "sns"]
+  lambda_mappings = [for item in local.mappings: item if lower(item.type) == "lambda"]
   caching_enabled = contains(local.mappings.*.cache, true)
 }
 
@@ -49,7 +51,8 @@ resource "aws_api_gateway_deployment" "api_gateway_deployment" {
     redeployment = sha1(join(",", [
       jsonencode([join(",", concat(module.s3_integrations.*.redeployment_trigger_json))]),
       jsonencode([join(",", concat(module.sns_integrations.*.redeployment_trigger_json))]),
-      jsonencode([join(",", concat(module.proxy_integrations.*.redeployment_trigger_json))])
+      jsonencode([join(",", concat(module.proxy_integrations.*.redeployment_trigger_json))]),
+      jsonencode([join(",", concat(module.lambda_integrations.*.redeployment_trigger_json))])
     ]))
   }
 
---
+++ modules/lambda-integration/variables.tf
@@ -0,0 +1,57 @@
+variable "rest_api_id" {
+  type        = string
+  description = "Rest API this path is attached to"
+}
+
+variable "path" {
+  type        = string
+  description = "Path from root of gateway"
+}
+
+variable "stage_name" {
+  type        = string
+  description = "AWS API gateway stage name"
+}
+
+variable "iam_role_arn" {
+  type        = string
+  description = "Gateway IAM role"
+}
+
+variable "env" {
+  type        = string
+  description = "Environment this is deployed in (e.g. dev or prod)"
+}
+
+variable "app_name" {
+  type        = string
+  description = "Self-descriptive"
+}
+
+variable "root_resource_id" {
+  type        = string
+  description = "Root api gateway path resource"
+}
+
+variable "custom_authorizer_id" {
+  type        = string
+  default     = ""
+  description = "Custom authorizer id for path"
+}
+
+variable "use_api_key" {
+  type        = bool
+  default     = false
+  description = "Whether to use custom api key"
+}
+
+variable "lamdba_invoke_arn" {
+  type        = string
+  description = "Endpoint to proxy to if not a VPC link"
+}
+
+variable "cache" {
+  type        = bool
+  default     = false
+  description = "Whether to use custom api key"
+}
