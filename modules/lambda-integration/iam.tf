d815a86e3c40db9b763daebc48c3e74e465f8362
Gabriel Chaney <gabriel.chaney@gmail.com>
2022-06-14 16:27:52 -0700

    Hardcore id suffix when proxy on root path

--- modules/lambda-integration/iam.tf
+++ modules/lambda-integration/iam.tf
@@ -1,9 +1,13 @@
 #################################
 # Api Gateway Lambda Invoke     #
 #################################
+locals {
+  statement_id_suffix = local.is_sub_path ? local.path_parts[0] : "root-path"
+  statement_id        = "${local.name_base}-lambda-invoke-${local.statement_id_suffix}"
+}
 
 resource "aws_lambda_permission" "lambda_permission" {
-  statement_id  = "${local.name_base}-lambda-invoke-${local.path_parts[0]}"
+  statement_id  = local.statement_id
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_name
   principal     = "apigateway.amazonaws.com"
