0dea61ed3ea9e5f6bd81437f4e02f1894d247e92
Gabriel Chaney <gabriel.chaney@gmail.com>
2022-06-15 12:30:55 -0700

    Allow for root path only path parts

--- modules/lambda-integration/main.tf
+++ modules/lambda-integration/main.tf
@@ -1,11 +1,11 @@
 locals {
   name_base           = "${var.env}-${var.app_name}"
   custom_auth         = var.custom_authorizer_id != ""
-  path_parts          = split("/", var.path)
+  path_parts          = compact(split("/", var.path))
   is_sub_path         = length(local.path_parts) > 1
+  start_path_part      = length(local.path_parts) == 0 ? "/" : local.path_parts[0]
   final_path_part     = element(local.path_parts, length(local.path_parts) - 1)
   has_proxy_path_part = local.final_path_part == "{proxy+}"
-
   root_resource_id = local.has_proxy_path_part ? aws_api_gateway_resource.proxy[0].id : var.root_resource_id
 }
 
@@ -13,7 +13,7 @@ resource "aws_api_gateway_resource" "path_root" {
   count       = local.is_sub_path ? 1 : 0
   rest_api_id = var.rest_api_id
   parent_id   = var.root_resource_id
-  path_part   = local.path_parts[0]
+  path_part   = local.start_path_part
 }
 
 resource "aws_api_gateway_resource" "proxy" {
